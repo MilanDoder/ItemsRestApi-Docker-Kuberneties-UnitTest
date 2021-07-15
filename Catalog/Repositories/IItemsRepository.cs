ba491cfded3808f5b28b4de28487b2af1db8e462
Doderovic Milan <mdoderovic5@gmail.com>
2021-07-15 08:35:07 +0200

    First init commit with rest api

---
+++ Catalog/Repositories/IItemsRepository.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Repositories
+{
+    public interface IItemsRepository
+    {
+        Task<Item> GetItemAsync(Guid id);
+        Task<IEnumerable<Item>> GetItemsAsync();
+
+        Task CreateItemAsync(Item item);
+        Task UpdateItemAsync(Item item);
+        Task DeleteItemAsync(Guid id);
+    }
+}
